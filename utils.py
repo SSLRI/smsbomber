4b4eb0be2c49e6d9533f4e28720b2a79fc45d54d
saeed salari <40920744+SSLRI@users.noreply.github.com>
2021-01-27 17:55:48 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,18 +1,18 @@
-# YetAnotherSMSBomber - A clean, small and powerful SMS bomber script.
+# smsbomber - A clean, small and powerful SMS bomber script.
 # Copyright (C) 2020 Avinash Reddy <https://github.com/sslri>
 #
-# YetAnotherSMSBomber is free software: you can redistribute it and/or modify
+# smsbomber is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# YetAnotherSMSBomber is distributed in the hope that it will be useful,
+# smsbomber is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with YetAnotherSMSBomber.  If not, see <https://www.gnu.org/licenses/>.
+# along with smsbomber.  If not, see <https://www.gnu.org/licenses/>.
 
 import argparse
 import os
